127b9263a2bbb361b4eb078847efab1eec19eb69
awe19001 <joeverlyncapute@gmail.com>
2020-01-18 21:45:27 +0800

    week2

--- updates.js
+++
@@ -1,8 +0,0 @@
-let options = {
-    day:"numeric",
-    month:"numeric",
-    year:"numeric"
-};
-let lastDate = new Date();
-document.getElementById('lastDate').textContent = lastDate.toLocaleDateString("en-US", options);
-document.getElementById('lastTime').textContent = lastDate.toLocaleTimeString("en-US", {hour12:false});
