a78eb554d08eb47b790bc4800f637f8f4fa38a76
awe19001 <joeverlyncapute@gmail.com>
2020-02-07 20:46:20 +0800

    lesson5

---
+++ Lesson5/js/toggle.js
@@ -0,0 +1,24 @@
+const hambutton = document.querySelector(".ham");
+hambutton.addEventListener("click", toggleMenu, false);
+
+
+function toggleMenu() {
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
+
+
+
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+const d = new Date()
+const dayName = days[d.getDay()]
+const day = new Date().getDate()
+
+
+const mlist = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+const m = new Date()
+const monthName = mlist[m.getMonth()]
+
+const year = new Date().getFullYear()
+
+
+document.getElementById("lastDate").textContent = `${dayName}, ${day} ${monthName} ${year}`;
