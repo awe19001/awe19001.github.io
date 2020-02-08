7562c80f692494ea466f2de7bb1bc814d2b36569
awe19001 <joeverlyncapute@gmail.com>
2020-02-08 12:07:40 +0800

    Lesson 5

--- Lesson5/js/toggle.js
+++ Lesson5/js/toggle.js
@@ -23,4 +23,11 @@ const year = new Date().getFullYear()
 
 document.getElementById("lastDate").textContent = `${dayName}, ${day} ${monthName} ${year}`;
 
-$('#donotAnnounce').show();else$('#announcement').show();
+function showThis() {
+    let today = new Date().getDay()
+    if (today == 5) {
+        document.getElementById("getNotice").style.display = "block";
+    } else {
+        document.getElementById("getNotice").style.display = "none";
+    };
+}
