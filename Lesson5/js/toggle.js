1dd1db29094f8d07665059d436c6cf3a760a5c5e
awe19001 <joeverlyncapute@gmail.com>
2020-02-14 08:01:12 +0800

    89

--- Lesson5/js/toggle.js
+++ Lesson5/js/toggle.js
@@ -3,7 +3,13 @@ hambutton.addEventListener("click", toggleMenu, false);
 
 
 function toggleMenu() {
-    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+    document.getElementsByClassName("navigation")[0].classList.toggle("hide");
+    let menuText = document.getElementsByClassName("menuNav")[0]
+    if (menuText.innerHTML == "☰ Menu") {
+        menuText.innerHTML = "X";
+    } else {
+        menuText.innerHTML = "&#9776; Menu";
+    }
 }
 
 
