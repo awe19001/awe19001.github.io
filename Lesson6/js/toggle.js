a7c7162109595edd6d75a7d846b7feac463c86a4
awe19001 <joeverlyncapute@gmail.com>
2020-02-14 08:57:08 +0800

    adlaw

--- Lesson6/js/toggle.js
+++ Lesson6/js/toggle.js
@@ -1,7 +1,3 @@
-const hambutton = document.querySelector(".ham");
-hambutton.addEventListener("click", toggleMenu, false);
-
-
 function toggleMenu() {
     document.getElementsByClassName("menubar")[0].classList.toggle("hide");
     let menuText = document.getElementsByClassName("menuText")[0]
