25efb960f0cbd9003c79a7fdef9a1ec47c007832
awe19001 <joeverlyncapute@gmail.com>
2020-02-16 04:52:26 +0800

    ambot lang

--- Lesson6/js/notice.js
+++ Lesson6/js/notice.js
@@ -1,46 +1,12 @@
-// let options = {
-//   day: "numeric",
-//   month: "numeric",
-//   year: "numeric"
-// };
+let lastModif = document.lastModified;
+document.getElementById('lastDate').innerHTML = lastModif;
 
-// let month = lastDate.getMonth();
-// let year = lastDate.getFullYear();
-// let dayMonth = lastDate.getDate()
-// let days = [
-//   "Sunday",
-//   "Monday",
-//   "Tuesday",
-//   "Wednesday",
-//   "Thursday",
-//   "Friday",
-//   "Saturday"
-// ]
-
-// let months = [
-//   "January", 
-//   "February", 
-//   "March", 
-//   "April", 
-//   "May", 
-//   "June", 
-//   "July", 
-//   "August",
-//   "September", 
-//   "October", 
-//   "November", 
-//   "December"
-// ]
-
-// let actualDate = days[day] + ", " + dayMonth + " " + months[month] + " " + year;
-// document.getElementById('lastDate').textContent = actualDate;
-
-function getNotice(){
-let lastDate = new Date();
-let adlaw = lastDate.getDay();
-if (day == 5) {
-  document.getElementById("getNotice").style.display = "block";
-} else {
-  document.getElementById("getNotice").style.display = "none";
+function Menu() {
+    document.getElementById("menu").classList.toggle("hide")
 }
+
+let d = new Date();
+let week = d.getDay();
+if(week !== 5) {
+    document.getElementById("getNotice").style.display = "none";
 }
--- Lesson6/js/toggle.js
+++ Lesson6/js/toggle.js
@@ -1,16 +1,6 @@
-// function toggleMenu() {
-//     document.getElementsByClassName("navigation")[0].classList.toggle("hide");
-//     let menuText = document.getElementsByClassName("menuNav")[0]
-//     if (menuText.innerHTML == "☰ Menu") {
-//         menuText.innerHTML = "X";
-//     } else {
-//         menuText.innerHTML = "&#9776; Menu";
-//     }
-// }
+const hambutton = document.querySelector(".ham");
+hambutton.addEventListener("click", toggleMenu, false);
 
 function toggleMenu() {
-    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+  document.querySelector(".navigation").classList.toggle("responsive");
 }
-
-
-
