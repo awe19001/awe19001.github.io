60040dc4f0a2de0a9dc8576033d4f1a5babeefb9
awe19001 <joeverlyncapute@gmail.com>
2020-02-16 09:16:39 +0800

    wind speed

--- Lesson6/js/windchill.js
+++ Lesson6/js/windchill.js
@@ -2,7 +2,7 @@ let high = parseFloat(document.getElementById("high").textContent);
 let windspeed = parseFloat(document.getElementById("windspeed").textContent);
 
 let windchill =
-  high <= 50 && windspeed > 3
+  high <= 55 && windspeed > 3
     ? (
         35.74 +  0.6215 * high -  35.75 * windspeed ** 0.16 + 0.4275 * high * windspeed ** 0.16
       ).toFixed(0)
