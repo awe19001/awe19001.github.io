f13cf7551e47a5309807dfc1deceb59cae51e6bf
awe19001 <joeverlyncapute@gmail.com>
2020-02-16 09:00:47 +0800

    date

--- Lesson6/js/lastDate.js
+++ Lesson6/js/lastDate.js
@@ -1,14 +1,12 @@
-const adlaw = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-const d = new Date()
-const dayName = adlaw[d.getDay()]
-const karon = new Date().getDate()
+let current = new Date();
+let day = current.getDay();
+let month = current.getMonth();
+let date = current.getDate();
+let year = current.getFullYear();
 
+let weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-const buwan = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-const m = new Date()
-const monthName = buwan[m.getMonth()]
+let currentDate = weekdays[day] + ", " + date + " " + months[month] + ", " + year;
 
-const tuig = new Date().getFullYear()
-
-
-document.getElementById("lastDate").textContent = `${dayName}, ${karon} ${monthName} ${tuig}`;
+document.getElementById("currentdate").textContent = currentDate;
