a46eb0f2d4d0d3ccb8e9c9c1dff5d4cf0a2a0c4d
awe19001 <joeverlyncapute@gmail.com>
2020-02-16 08:56:16 +0800

    wing

--- Lesson6/js/windchill.js
+++ Lesson6/js/windchill.js
@@ -1,18 +1,11 @@
-function getWindChill()
-{
-    var temp = parseFloat(document.getElementById('temp').innerHTML);
-    var windspeed = parseFloat(document.getElementById('windspeed').innerHTML);
-    var windchill = document.getElementById('windchill');
+let high = parseFloat(document.getElementById("high").textContent);
+let windspeed = parseFloat(document.getElementById("windspeed").textContent);
 
-    if (temp > 50 || windspeed < 3) {
-        windchill.innerHTML = "N/A";
-    }else{
-        calculateWindchill(temp,windspeed);
-    }
-}
+let windchill =
+  high <= 50 && windspeed > 3
+    ? (
+        35.74 +  0.6215 * high -  35.75 * windspeed ** 0.16 + 0.4275 * high * windspeed ** 0.16
+      ).toFixed(0)
+    : "N/A";
 
-function calculateWindchill(temp,windspeed)
-{
-    var result = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(windspeed,0.16)) + (0.4275 * temp * Math.pow(windspeed,0.16));
-    windchill.innerHTML = parseFloat(result.toFixed(2));
-}
+document.getElementById("windchill").textContent = windchill;
