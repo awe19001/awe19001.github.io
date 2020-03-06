e5d204d3d3c219cbc89bf409c07b20b43789df19
awe19001 <joeverlyncapute@gmail.com>
2020-03-06 08:02:18 +0800

    weather

---
+++ lesson9/js/index.js
@@ -0,0 +1,31 @@
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+fetch(requestURL)
+    .then(function (response) {
+        //console.Log(response);
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        //console.table(jsonObject);
+        const towns = jsonObject['towns'];
+        console.table(jsonObject);
+        for (let i = 0; i < towns.length; i++) {
+            let card = document.createElement('section');
+            let h2 = document.createElement('h2');
+            let image = document.createElement('img');
+            let div1 = document.createElement('div');
+            let div2 = document.createElement('div');
+   
+            h2.textContent = towns[i].name + ' ' + towns[i].lastname;
+
+            div1.textContent = 'Name: ' + towns[i].name;
+            div2.textContent = 'Motto: ' + towns[i].motto;
+            image.setAttribute('src', towns[i].imageurl);
+            image.setAttribute('alt', towns[i].name + ' ' + towns[i].lastname + ' - ' + towns[i].order)
+
+            card.appendChild(h2);
+            card.appendChild(div1);
+            card.appendChild(div2);
+            card.appendChild(image);
+
+            document.querySelector('div.cards').appendChild(card);
+        }});
