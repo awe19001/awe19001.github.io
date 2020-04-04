665b5f81eb66a49447a580b19e02c6a42a68587e
awe19001 <joeverlyncapute@gmail.com>
2020-04-04 03:54:27 -0600

    bio

--- lesson13/js/professionalguides.js
+++ lesson13/js/professionalguides.js
@@ -6,7 +6,7 @@ fetch(requestURL)
     })
     .then(function (jsonObject) {
         //console.table(jsonObject);
-        const prophets = jsonObject['professional'];
+        const professional = jsonObject['professional'];
         console.table(jsonObject);
         for (let i = 0; i < professional.length; i++) {
             let card = document.createElement('section');
@@ -14,17 +14,20 @@ fetch(requestURL)
             let image = document.createElement('img');
             let div1 = document.createElement('div');
             let div2 = document.createElement('div');
+            let div3 = document.createElement('div');
               
             h2.textContent = professional[i].name;
 
             div1.textContent = 'Certification Level: ' + professional[i].level;
             div2.textContent = 'Number of Years: ' + professional[i].years;
+            div3.textContent = 'Email Address: ' + professional[i].email;
             image.setAttribute('src', professional[i].imageurl);
-            image.setAttribute('alt', professional[i].name + ' ' + professional[i].name + ' - ' + professional[i].order)
+            image.setAttribute('alt', professional[i].name)
 
             card.appendChild(h2);
             card.appendChild(div1);
             card.appendChild(div2);
+            card.appendChild(div3);
  
             card.appendChild(image);
 
