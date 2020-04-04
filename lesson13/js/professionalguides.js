da06783c6139fe6e4bf712b3b1356d42af925b0b
awe19001 <joeverlyncapute@gmail.com>
2020-04-04 01:48:37 -0600

    updates js

--- lesson13/js/professionalguides.js
+++ lesson13/js/professionalguides.js
@@ -6,25 +6,28 @@ fetch(requestURL)
     })
     .then(function (jsonObject) {
         //console.table(jsonObject);
-        const prophets = jsonObject['prophets'];
+        const prophets = jsonObject['professional'];
         console.table(jsonObject);
-        for (let i = 0; i < prophets.length; i++) {
+        for (let i = 0; i < professional.length; i++) {
             let card = document.createElement('section');
             let h2 = document.createElement('h2');
             let image = document.createElement('img');
             let div1 = document.createElement('div');
             let div2 = document.createElement('div');
+            let div3 = document.createElement('div');
    
-            h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+            h2.textContent = professional[i].name;
 
-            div1.textContent = 'Date of Birth: ' + prophets[i].birthdate;
-            div2.textContent = 'Place of Birth: ' + prophets[i].birthplace;
-            image.setAttribute('src', prophets[i].imageurl);
-            image.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname + ' - ' + prophets[i].order)
+            div1.textContent = 'Certification Level: ' + professional[i].level;
+            div2.textContent = 'Number of Years: ' + professional[i].years;
+            div3.textContent = 'Email-address: ' + professional[i].email;
+            image.setAttribute('src', professional[i].imageurl);
+            image.setAttribute('alt', professional[i].name + ' ' + professional[i].name + ' - ' + professional[i].order)
 
             card.appendChild(h2);
             card.appendChild(div1);
             card.appendChild(div2);
+            card.appendChild(div3);
             card.appendChild(image);
 
             document.querySelector('div.cards').appendChild(card);
